7b0a54b4b5e8ac9980bc8296332bdba4ad07562b
Santiago Rhenals <santiagorhenals@gmail.com>
2020-03-29 18:41:49 +0200

    feat: Added a show persons button styles: Improved UI

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: 'John', age: 28 },
       { name: "Estefania", age: 16 },
       { name: "Brian", age: 22 },
-    ]
+    ],
+    showPersons: false,
   }
 
   // Used to switch the name of the persons
@@ -22,7 +23,7 @@ class App extends Component {
       ]
     })
   }
-
+  // Change the name by typing
   nameChangeHandler = event => {
     this.setState({
       persons: [
@@ -33,30 +34,48 @@ class App extends Component {
     })
   }
 
+  // Hide/Show the persons elements
+  togglePersonsHandler = () => {
+
+    this.setState({
+      showPersons: !this.state.showPersons,
+    })
+  }
+
   // Rendering the component
   render() {
+    // Variable used to define if the persons will be shown or not
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            change={this.nameChangeHandler}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Dante')}
+            change={this.nameChangeHandler}
+          />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+            change={this.nameChangeHandler}
+          >My hobbies are: Being cool
+            </Person>
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Simple React App</h1>
         <h2>List of persons</h2>
-        <button className="changeButton" onClick={ () => this.switchNameHandler('Maria')}>Switch name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} 
-          change={this.nameChangeHandler}
-          />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Dante')} 
-          change={this.nameChangeHandler}
-          />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          change={this.nameChangeHandler}
-          >My hobbies are: Being cool
-          </Person>
+        <button className="changeButton" onClick={this.togglePersonsHandler}>Show persons</button>
+        {persons}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const person = (props) => {
         <div className="Person">
             <p onClick={props.click}>My name is {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.change} value={props.name}></input>
+            <input className="usernameInput" type="text" onChange={props.change} value={props.name}></input>
         </div>
     )
 };
