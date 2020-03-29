c3c6993826067f381a8a3edef08f0b734a280be9
Santiago Rhenals <santiagorhenals@gmail.com>
2020-03-29 14:26:33 +0200

    Added hooks to App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const App = (props) => {
+
+  const [ personsState, setPersons ] = useState({
     persons: [
       { name: 'John', age: 28 },
       { name: "Estefania", age: 16 },
       { name: "Brian", age: 22 },
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    console.log('Was clicked');
-    this.setState({
-      persons: [
-        { name: 'Delia', age: 50 },
-        { name: "Estefania", age: 16 },
-        { name: "Schwanti", age: 20 },
-      ]
-    })
-  }
+  const switchNameHandler = () => {
+      console.log('Was clicked');
+      setPersons({
+        persons: [
+          { name: 'Delia', age: 50 },
+          { name: "Estefania", age: 16 },
+          { name: "Schwanti", age: 20 },
+        ]
+      })
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi there</h1>
-        <h2>Hi here</h2>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies are: Being cool</Person>
-      </div>
-    );
+  return (
+    <div className="App">
+      <h1>Hi there</h1>
+      <h2>Hi here</h2>
+      <button onClick={switchNameHandler}>Switch name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>My hobbies are: Being cool</Person>
+    </div>
+  );
+
   }
-}
 
 export default App;
