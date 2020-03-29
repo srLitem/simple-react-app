10243e1750cdeb4f53c6dca899e308520f8256eb
Santiago Rhenals <santiagorhenals@gmail.com>
2020-03-29 14:09:21 +0200

    Added setState to App.js when clicking the update button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi there</h1>
-      <h2>Hi here</h2>
-      <Person name="Paula" age="20"/>
-      <Person name="Estefania" age="16"/>
-      <Person name="Brian" age="22">My hobbies are: Being cool</Person>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'John', age: 28 },
+      { name: "Estefania", age: 16 },
+      { name: "Brian", age: 22 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked');
+    this.setState({
+      persons: [
+        { name: 'Delia', age: 50 },
+        { name: "Estefania", age: 16 },
+        { name: "Schwanti", age: 20 },
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi there</h1>
+        <h2>Hi here</h2>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies are: Being cool</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
