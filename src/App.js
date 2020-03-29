986755c0004f9c8de2bcf066f5f4f6fe9615b5fe
Santiago Rhenals <santiagorhenals@gmail.com>
2020-03-29 15:41:11 +0200

    Added a name variable support to the switchVariableName

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  //Setting the state up
   state = {
     persons: [
       { name: 'John', age: 28 },
@@ -11,26 +12,35 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  // Used to switch the name of the persons
+  switchNameHandler = (newName) => {
     console.log('Was clicked');
     this.setState({
       persons: [
         { name: 'Delia', age: 50 },
-        { name: "Estefania", age: 16 },
+        { name: newName, age: 16 },
         { name: "Schwanti", age: 20 },
       ]
     })
   }
 
+  // Rendering the component
   render() {
     return (
       <div className="App">
         <h1>Hi there</h1>
         <h2>Hi here</h2>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies are: Being cool</Person>
+        <button onClick={ () => this.switchNameHandler('Maria')}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Dante')} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}>My hobbies are: Being cool</Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>Super simple text from a person of {props.age} years old</p>
-            <p>Btw, my name is {props.name}</p>
+            <p onClick={props.click}>My name is {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
         </div>
     )
